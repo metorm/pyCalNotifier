08f55d30e21d4f8502678d63b790085f0a27f764
Wei Ran <metorm@outlook.com>
2021-04-01 11:25:28 +0800

    Retry after net work failure.

--- pyCalNotifier/getTodoItems.py
+++ pyCalNotifier/getTodoItems.py
@@ -1,9 +1,10 @@
 import configparser
+import time
 import caldav
 from ics import Calendar
 
 
-def getTodoItems(configFile):
+def getTodoItems(configFile, maxRetry=60, retryInterval=60):
     config = configparser.ConfigParser()
     config.read(configFile, encoding='utf-8')
 
@@ -12,14 +13,29 @@ def getTodoItems(configFile):
     config_user = config["server"]["user"]
     config_pw = config["server"]["pw"]
 
-    # connection
-    client = caldav.DAVClient(
-        url=config_url, username=config_user, password=config_pw)
+    retryCount = 0
+    while(True):
+        retryCount = retryCount + 1
 
-    # parse
-    for calendar in client.principal().calendars():
-        if calendar.name != config_calendarName:
-            continue
+        if retryCount > maxRetry:
+            print("重试了%d次，都出问题了，自己想办法吧……" % retryCount)
+            break
 
-        AllTodos = calendar.todos()
-        return [todo for todoSet in AllTodos for todo in Calendar(todoSet.data).todos]
+        try:
+
+            # connection
+            client = caldav.DAVClient(
+                url=config_url, username=config_user, password=config_pw)
+
+            # parse
+            for calendar in client.principal().calendars():
+                if calendar.name != config_calendarName:
+                    continue
+
+                AllTodos = calendar.todos()
+                return [todo for todoSet in AllTodos for todo in Calendar(todoSet.data).todos]
+        except:
+            print("网络异常，%d秒后重试……" % retryInterval)
+            time.sleep(retryInterval)
+
+    return None
